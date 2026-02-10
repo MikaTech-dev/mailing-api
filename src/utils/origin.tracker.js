f611eb8f65d92df06e3c0b6804a9a7d75c819982
MikaTech-dev <verasamoma@gmail.com>
2026-02-10 05:09:53 +0100

    feat/created verify.origin.js - verify.origin.js controls what origins are and arent allowed according to origins predefined in the .env as a string - streamlining fs and file naming

---
+++ src/utils/origin.tracker.js
@@ -0,0 +1,38 @@
+import "dotenv/config"
+
+const websites = process.env.WEBSITES
+const websiteArray = websites.split(",")
+
+export const verifyTraffic = async ()=>{
+    // WIthout promise
+
+    // function verify () {if (websiteArray.includes(req.hostame)) return true; else {return false};}
+    function verify() {
+        if (websiteArray.includes("localhost")) {
+            console.log("Includes indeed")
+            return true
+        }
+        else throw new Error ("Bad Host")
+    }
+    console.log(verify())
+    // return new Promise((resolve, reject)=>{
+    //     if (verify()) {
+    //         resolve(verify)
+    //     }
+    //     reject(new Error ("Bad IP"))
+    // })
+    
+}
+
+export const verifyHostname = async (req, res) => {
+    return new Promise ((resolve, reject) => {
+        const reqSource = req.hostname
+        if (websiteArray.includes(reqSource)){
+            console.log(`Requests from ${reqSource}`)
+            resolve("Valid hostname")   // True if req source is allowed
+        }else {
+            console.log (`Request from ${reqSource} couldnt be fulfilled`)
+            reject(new Error ("Host not allowed"))
+        }
+    })
+}
