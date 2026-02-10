c30d0d21a8115b4e1e815e1c1f6574fc6519290d
MikaTech-dev <verasamoma@gmail.com>
2026-02-10 05:04:25 +0100

    fix/ repositioned around morgan to log for all routes

--- app.js
+++ app.js
@@ -2,13 +2,11 @@
 import "dotenv/config"
 import express from "express"
 import morgan from "morgan"
-import router from "./src/routes.js"
-import { verifyTransport } from "./src/services/email/nodemail.handler.js"
+import router from "./src/services/routing/routes.js"
+import { verifyGoogleTransport } from "./src/services/email/nodemail.handler.js"
 // const cors = require ("cors")
 
 const app = express()
-// Morgan config
-app.use(morgan("dev"))
 
 // Cors
 
@@ -18,16 +16,21 @@ app.use(morgan("dev"))
 //     }))
 // }
 
+// Morgan config
+app.use(morgan("dev"))
+
+app.use(express.json())
+app.use("/api", router)
+
+
 // Server config and startup
 const PORT = process.env.NODE_PORT || 5000
 
 app.listen (PORT, ()=> {
     try {
         console.log (`☑️  Server started at http://localhost:${PORT}`)
-        verifyTransport() // Verify SMTP connection on startup.
+        verifyGoogleTransport() // Verify SMTP connection on startup.
     } catch (error) {
         console.log ("🚫  Error occurred while starting the server", error)
     }
 })
-app.use(express.json())
-app.use("/api", router)
